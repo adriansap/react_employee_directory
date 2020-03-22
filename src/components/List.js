6e07490d66fdbd7fb2e5a6f631d48b214b362b4c
Adrian S <adriansaporta@gmail.com>
2020-03-21 22:08:29 -0400

    an MVP

--- src/components/List.js
+++ src/components/List.js
@@ -1,14 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
+
+
+
+
+
 // Whenever we try to render an array containing JSX, React knows to render each JSX element separately
 function List(props) {
+  const [queryValue, setTitle] = useState('')
+
   console.log("props.employees[0].image" + props.employees[0].image)
 
   return (
     <div class="md-form active-pink active-pink-2 mb-3 mt-0">
-      <input class="form-control" type="text" placeholder="Search" aria-label="Search"></input>
+      <h1>Employee Directory</h1>
+      <input onChange={event => setTitle(event.target.value)} class="form-control" type="text" placeholder="Search" aria-label="Search"></input>
+
       <table class="table">
         <thead>
-          <tr>
+          {/* <tr>
             <th scope="col">#</th>
             <th scope="col">Image</th>
             <th scope="col">Name</th>
@@ -16,49 +25,61 @@ function List(props) {
             <th scope="col">Email</th>
             <th scope="col">DOB</th>
 
-          </tr>
+          </tr> */}
         </thead>
         <tbody>
 
-        <th scope="row">{props.employees.id}</th>
+          {/* <th scope="row">{props.employees.id}</th> */}
 
+          <tr>
+            <th scope="row">Pic</th>
+            {props.employees.filter(name => name.name.includes(queryValue)).map(filteredName => (
 
-          {props.employees.filter(name => name.name.includes('su')).map(filteredName => (
+              <td>
+                <img src={filteredName.image}></img>
+              </td>
+            ))}
+          </tr>
 
-            <td>
-              {filteredName.image}
-            </td>
-          ))}
+          <tr>
+            <th scope="row">Name</th>
+            {props.employees.filter(name => name.name.includes(queryValue)).map(filteredName => (
 
+              <td>
+                {filteredName.name}
+              </td>
+            ))}
+          </tr>
 
-          {props.employees.filter(name => name.name.includes('su')).map(filteredName => (
+          <tr>
+            <th scope="row">Phone</th>
+            {props.employees.filter(name => name.name.includes(queryValue)).map(filteredName => (
 
-            <td>
-              {filteredName.name}
-            </td>
-          ))}
+              <td>
+                {filteredName.phone}
+              </td>
+            ))}
+          </tr>
 
-          {props.employees.filter(name => name.name.includes('su')).map(filteredName => (
+          <tr>
+            <th scope="row">Email</th>
+            {props.employees.filter(name => name.name.includes(queryValue)).map(filteredName => (
 
-            <td>
-              {filteredName.phone}
-            </td>
-          ))}
+              <td>
+                {filteredName.email}
+              </td>
+            ))}
+          </tr>
 
-          {props.employees.filter(name => name.name.includes('su')).map(filteredName => (
+          <tr>
+            <th scope="row">DOB</th>
+            {props.employees.filter(name => name.name.includes(queryValue)).map(filteredName => (
 
-            <td>
-              {filteredName.email}
-            </td>
-          ))}
-
-          {props.employees.filter(name => name.name.includes('su')).map(filteredName => (
-
-            <td>
-              {filteredName.DOB}
-            </td>
-          ))}
-
+              <td>
+                {filteredName.DOB}
+              </td>
+            ))}
+          </tr>
 
 
 
