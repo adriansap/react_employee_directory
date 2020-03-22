359c19e137a0aec78f3b071b7155bff36c8c4805
Adrian S <adriansaporta@gmail.com>
2020-03-21 23:26:08 -0400

    list in proper format

--- src/components/List.js
+++ src/components/List.js
@@ -1,23 +1,19 @@
 import React, { useState } from "react";
 
-
-
-
-
 // Whenever we try to render an array containing JSX, React knows to render each JSX element separately
 function List(props) {
-  const [queryValue, setTitle] = useState('')
+  const [queryValue, setValue] = useState('') //until setValue sets a value, queryValue = ''
 
   console.log("props.employees[0].image" + props.employees[0].image)
 
   return (
     <div class="md-form active-pink active-pink-2 mb-3 mt-0">
       <h1>Employee Directory</h1>
-      <input onChange={event => setTitle(event.target.value)} class="form-control" type="text" placeholder="Search" aria-label="Search"></input>
+      <input onChange={event => setValue(event.target.value)} class="form-control" type="text" placeholder="Search" aria-label="Search"></input>
 
       <table class="table">
         <thead>
-          {/* <tr>
+          <tr>
             <th scope="col">#</th>
             <th scope="col">Image</th>
             <th scope="col">Name</th>
@@ -25,63 +21,21 @@ function List(props) {
             <th scope="col">Email</th>
             <th scope="col">DOB</th>
 
-          </tr> */}
+          </tr>
         </thead>
         <tbody>
 
-          {/* <th scope="row">{props.employees.id}</th> */}
 
-          <tr>
-            <th scope="row">Pic</th>
-            {props.employees.filter(name => name.name.toLowerCase().includes(queryValue)).map(filteredName => (
+          {props.employees.filter(name => name.name.toLowerCase().includes(queryValue)).map(filteredName => (
 
-              <td>
-                <img src={filteredName.image} alt="profilepic"></img>
-              </td>
-            ))}
-          </tr>
-
-          <tr>
-            <th scope="row">Name</th>
-            {props.employees.filter(name => name.name.toLowerCase().includes(queryValue)).map(filteredName => (
-
-
-              <td>
-                {filteredName.name}
-              </td>
-            ))}
-          </tr>
-
-          <tr>
-            <th scope="row">Phone</th>
-            {props.employees.filter(name => name.name.toLowerCase().includes(queryValue)).map(filteredName => (
-
-              <td>
-                {filteredName.phone}
-              </td>
-            ))}
-          </tr>
-
-          <tr>
-            <th scope="row">Email</th>
-            {props.employees.filter(name => name.name.toLowerCase().includes(queryValue)).map(filteredName => (
-
-              <td>
-                {filteredName.email}
-              </td>
-            ))}
-          </tr>
-
-          <tr>
-            <th scope="row">DOB</th>
-            {props.employees.filter(name => name.name.toLowerCase().includes(queryValue)).map(filteredName => (
-
-              <td>
-                {filteredName.DOB}
-              </td>
-            ))}
-          </tr>
-
+            <tr>
+              <th scope="row"></th>
+              <td><img src={filteredName.image} alt="profilepic"></img></td>
+              <td>{filteredName.phone}</td>
+              <td>{filteredName.email}</td>
+              <td>{filteredName.DOB}</td>
+            </tr>
+          ))}
 
 
         </tbody>
